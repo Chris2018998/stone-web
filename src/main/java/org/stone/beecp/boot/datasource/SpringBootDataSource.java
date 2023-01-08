032f6fc5ff90d677084bab522470d7a873052e0a
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2023-01-08 22:05:25 +0800

    Add files via upload

---
+++ src/main/java/org/stone/beecp/boot/datasource/SpringBootDataSource.java
@@ -0,0 +1,184 @@
+/*
+ * Copyright Chris2018998
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.stone.beecp.boot.datasource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.stone.beecp.BeeDataSource;
+import org.stone.beecp.boot.datasource.statement.StatementTraceUtil;
+import org.stone.beecp.jta.BeeJtaDataSource;
+import org.stone.beecp.pool.ConnectionPoolMonitorVo;
+
+import javax.sql.DataSource;
+import java.io.PrintWriter;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.UUID;
+
+import static org.stone.beecp.boot.datasource.SpringBootDataSourceUtil.tryToCloseDataSource;
+
+/**
+ * statement datasource
+ *
+ * @author Chris.Liao
+ */
+public class SpringBootDataSource implements DataSource {
+    private final static Logger Log = LoggerFactory.getLogger(SpringBootDataSource.class);
+    private final String dsId;
+    private final String dsUUID;
+    private final DataSource ds;
+    private final boolean jndiDs;
+    private final boolean isBeeDs;
+
+    private boolean primary;
+    private boolean traceSql;
+    private Method poolClearMethod;
+    private Method poolMonitorVoMethod;
+    private boolean notSetBeeDsId = true;
+
+    SpringBootDataSource(String dsId, DataSource ds, boolean jndiDs) {
+        this.dsId = dsId;
+        this.ds = ds;
+        this.jndiDs = jndiDs;
+
+        this.isBeeDs = ds instanceof BeeDataSource || ds instanceof BeeJtaDataSource;
+        if (isBeeDs) readBeeDsMethods();
+        this.dsUUID = "SpringDs_" + UUID.randomUUID().toString();
+    }
+
+    String getDsId() {
+        return dsId;
+    }
+
+    boolean isPrimary() {
+        return primary;
+    }
+
+    void setPrimary(boolean primary) {
+        this.primary = primary;
+    }
+
+    void setTraceSql(boolean traceSql) {
+        this.traceSql = traceSql;
+    }
+
+    public Connection getConnection() throws SQLException {
+        Connection con = ds.getConnection();
+        return traceSql ? StatementTraceUtil.createConnection(con, dsId, dsUUID) : con;
+    }
+
+    public Connection getConnection(String username, String password) throws SQLException {
+        Connection con = ds.getConnection(username, password);
+        return traceSql ? StatementTraceUtil.createConnection(con, dsId, dsUUID) : con;
+    }
+
+    public PrintWriter getLogWriter() throws SQLException {
+        return ds.getLogWriter();
+    }
+
+    public void setLogWriter(PrintWriter out) throws SQLException {
+        ds.setLogWriter(out);
+    }
+
+    public int getLoginTimeout() throws SQLException {
+        return ds.getLoginTimeout();
+    }
+
+    public void setLoginTimeout(int seconds) throws SQLException {
+        ds.setLoginTimeout(seconds);
+    }
+
+    public java.util.logging.Logger getParentLogger() throws SQLFeatureNotSupportedException {
+        return ds.getParentLogger();
+    }
+
+    public boolean isWrapperFor(Class<?> clazz) {
+        return clazz != null && clazz.isInstance(this);
+    }
+
+    public <T> T unwrap(Class<T> clazz) throws SQLException {
+        if (clazz != null && clazz.isInstance(this))
+            return clazz.cast(this);
+        else
+            throw new SQLException("Wrapped object was not an instance of " + clazz);
+    }
+
+    void close() {
+        if (!jndiDs) tryToCloseDataSource(ds);
+    }
+
+    void clearAllConnections() {
+        if (poolClearMethod != null) {
+            try {
+                poolClearMethod.invoke(ds, false);
+            } catch (Throwable e) {
+                Log.warn("Failed to execute dataSource 'clear' method", e);
+            }
+        }
+    }
+
+    ConnectionPoolMonitorVo getPoolMonitorVo() {
+        if (poolMonitorVoMethod != null) {
+            try {
+                ConnectionPoolMonitorVo vo = (ConnectionPoolMonitorVo) poolMonitorVoMethod.invoke(ds);
+                if (notSetBeeDsId) setBeeDsIdToMonitorSingletonVo(vo);
+                return vo;
+            } catch (Throwable e) {
+                Log.warn("Failed to execute dataSource 'getPoolMonitorVo' method", e);
+            }
+        }
+        return null;
+    }
+
+    private synchronized void setBeeDsIdToMonitorSingletonVo(ConnectionPoolMonitorVo vo) {
+        setValueToField(vo, "dsId", dsId);
+        setValueToField(vo, "dsUUID", dsUUID);
+        notSetBeeDsId = false;
+    }
+
+    private void setValueToField(ConnectionPoolMonitorVo vo, String fieldId, String value) {
+        Field field = null;
+        try {
+            Class monitorVoClass = ConnectionPoolMonitorVo.class;
+            field = monitorVoClass.getDeclaredField(fieldId);
+            field.setAccessible(true);
+            field.set(vo, value);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            //do nothing
+        } finally {
+            if (field != null) field.setAccessible(false);
+        }
+    }
+
+    private void readBeeDsMethods() {
+        if (isBeeDs) {
+            Class dsClass = ds.getClass();
+            try {
+                poolMonitorVoMethod = dsClass.getMethod("getPoolMonitorVo");
+            } catch (NoSuchMethodException e) {
+                Log.warn("DataSource method(getPoolMonitorVo) not found", e);
+            }
+            try {
+                poolClearMethod = dsClass.getMethod("clear", Boolean.TYPE);
+            } catch (NoSuchMethodException e) {
+                Log.warn("DataSource method(clear) not found", e);
+            }
+        }
+    }
+}
---
+++ src/main/java/org/stone/beecp/boot/datasource/SpringBootDataSourceManager.java
@@ -0,0 +1,205 @@
+/*
+ * Copyright Chris2018998
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.stone.beecp.boot.datasource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.stone.beecp.boot.datasource.monitor.redis.RedisPushTask;
+import org.stone.beecp.boot.datasource.statement.StatementTrace;
+import org.stone.beecp.boot.datasource.statement.StatementTraceAlert;
+import org.stone.beecp.pool.ConnectionPoolMonitorVo;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.sql.Statement;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static org.stone.util.CommonUtil.isBlank;
+
+/*
+ * DataSource Manager
+ *
+ * @author Chris.Liao
+ */
+public class SpringBootDataSourceManager {
+    private final static SpringBootDataSourceManager instance = new SpringBootDataSourceManager();
+    private final Map<String, SpringBootDataSource> dsMap;
+    private final ScheduledThreadPoolExecutor timerExecutor;
+    private final Logger Log = LoggerFactory.getLogger(SpringBootDataSourceManager.class);
+
+    private boolean sqlShow;
+    private boolean sqlTrace;
+    private long sqlExecSlowTime;
+    private long sqlTraceTimeout;
+    private int sqlTraceMaxSize;
+    private StatementTraceAlert sqlTraceAlert;
+    private AtomicInteger sqlTracedSize;
+    private ConcurrentLinkedDeque<StatementTrace> sqlTraceQueue;
+
+    private SpringBootDataSourceManager() {
+        this.dsMap = new ConcurrentHashMap<>(1);
+        timerExecutor = new ScheduledThreadPoolExecutor(2, new SpringBootDsThreadFactory());
+        timerExecutor.setKeepAliveTime(15, TimeUnit.SECONDS);
+        timerExecutor.allowCoreThreadTimeOut(true);
+    }
+
+    public static SpringBootDataSourceManager getInstance() {
+        return instance;
+    }
+
+    SpringBootDataSource getSpringBootDataSource(String dsId) {
+        return dsMap.get(dsId);
+    }
+
+    void addSpringBootDataSource(SpringBootDataSource ds) {
+        dsMap.put(ds.getDsId(), ds);
+        ds.setTraceSql(sqlTrace);
+    }
+
+    //create sql statement pool
+    void setupMonitorConfig(DataSourceMonitorConfig config) {
+        if (sqlTrace = config.isSqlTrace()) {
+            this.sqlShow = config.isSqlShow();
+            this.sqlExecSlowTime = config.getSqlExecSlowTime();
+            this.sqlTraceMaxSize = config.getSqlTraceMaxSize();
+            this.sqlTraceTimeout = config.getSqlTraceTimeout();
+            this.sqlTraceAlert = config.getSqlExecAlertAction();
+            this.sqlTracedSize = new AtomicInteger(0);
+            this.sqlTraceQueue = new ConcurrentLinkedDeque<>();
+            //sql trace timeout scan
+            timerExecutor.scheduleAtFixedRate(new SqlTraceTimeoutTask(), 0, config.getSqlTraceTimeoutScanPeriod(), MILLISECONDS);
+
+            String redisHost = config.getRedisHost();
+            if (!isBlank(redisHost)) {//send datasource info to redis
+                JedisPoolConfig redisConfig = new JedisPoolConfig();
+                redisConfig.setMinIdle(0);
+                redisConfig.setMaxTotal(1);
+                JedisPool pool = new JedisPool(redisConfig, redisHost, config.getRedisPort(), config.getRedisTimeoutMs(), config.getRedisUserId(), config.getRedisPassword());
+
+                int expireSeconds = (int) MILLISECONDS.toSeconds(config.getRedisSendPeriod());
+                timerExecutor.scheduleAtFixedRate(new RedisPushTask(pool, expireSeconds), 0, config.getRedisSendPeriod(), MILLISECONDS);
+            }
+        }
+    }
+
+    //clear pool
+    public void clearDsConnections(String dsId) {
+        SpringBootDataSource ds = dsMap.get(dsId);
+        if (ds != null) ds.clearAllConnections();
+    }
+
+    //get sql statement list
+    public Collection<StatementTrace> getSqlExecutionList() {
+        return sqlTraceQueue;
+    }
+
+    //get pool connection monitor
+    public List<ConnectionPoolMonitorVo> getPoolMonitorVoList() {
+        List<ConnectionPoolMonitorVo> poolMonitorVoList = new ArrayList<>(dsMap.size());
+        Iterator<SpringBootDataSource> iterator = dsMap.values().iterator();
+        while (iterator.hasNext()) {
+            SpringBootDataSource ds = iterator.next();
+            ConnectionPoolMonitorVo vo = ds.getPoolMonitorVo();
+            if (vo == null) continue;
+            if (vo.getPoolState() == 2) {//POOL_CLOSED
+                iterator.remove();
+            } else {
+                poolMonitorVoList.add(vo);
+            }
+        }
+        return poolMonitorVoList;
+    }
+
+    //add statement sql
+    public Object traceSqlExecution(StatementTrace vo, Statement statement, Method method, Object[] args) throws Throwable {
+        vo.setMethodName(method.getName());
+        sqlTraceQueue.offerFirst(vo);
+        if (sqlTracedSize.incrementAndGet() > sqlTraceMaxSize) {
+            sqlTraceQueue.pollLast();
+            sqlTracedSize.decrementAndGet();
+        }
+
+        try {
+            if (sqlShow) Log.info("Executing sql:{}", vo.getSql());
+            Object re = method.invoke(statement, args);
+            vo.setSuccessInd(true);
+            return re;
+        } catch (InvocationTargetException e) {
+            vo.setSuccessInd(false);
+            Throwable failedCause = e.getCause();
+            if (failedCause == null) failedCause = e;
+            vo.setFailCause(failedCause);
+            throw failedCause;
+        } catch (Throwable e) {
+            vo.setSuccessInd(false);
+            vo.setFailCause(e);
+            throw e;
+        } finally {
+            Date endDate = new Date();
+            vo.setEndTimeMs(endDate.getTime());
+            vo.setEndTime(SpringBootDataSourceUtil.formatDate(endDate));
+            vo.setTookTimeMs(vo.getEndTimeMs() - vo.getStartTimeMs());
+            if (vo.isSuccessInd() && vo.getTookTimeMs() >= sqlExecSlowTime)//alert
+                vo.setSlowInd(true);
+        }
+    }
+
+    private void removeTimeoutTrace(LinkedList<StatementTrace> sqlAlertTempList) {
+        Iterator<StatementTrace> iterator = sqlTraceQueue.descendingIterator();
+        while (iterator.hasNext()) {
+            StatementTrace vo = iterator.next();
+            if (vo.getEndTimeMs() > 0 && (!vo.isSuccessInd() || vo.isSlowInd()) && !vo.isAlertedInd()) {//failed or slow
+                vo.setAlertedInd(true);
+                sqlAlertTempList.add(vo);
+                if (sqlShow) Log.info("{} sql:{}", vo.isSlowInd() ? "Slow" : "Error", vo.getSql());
+            }
+
+            if (System.currentTimeMillis() - vo.getStartTimeMs() >= sqlTraceTimeout) {
+                iterator.remove();
+                sqlTracedSize.decrementAndGet();
+            }
+        }
+
+        if (!sqlAlertTempList.isEmpty()) { //should be in short time
+            try {
+                sqlTraceAlert.alert(sqlAlertTempList);
+            } finally {
+                sqlAlertTempList.clear();
+            }
+        }
+    }
+
+    private static final class SpringBootDsThreadFactory implements ThreadFactory {
+        public Thread newThread(Runnable r) {
+            Thread th = new Thread(r, "SpringBootDsThreadFactory");
+            th.setDaemon(true);
+            return th;
+        }
+    }
+
+    private static final class SqlTraceTimeoutTask implements Runnable {
+        private LinkedList<StatementTrace> sqlAlertTempList = new LinkedList<>();
+
+        public void run() {// check idle connection
+            instance.removeTimeoutTrace(sqlAlertTempList);
+        }
+    }
+}
